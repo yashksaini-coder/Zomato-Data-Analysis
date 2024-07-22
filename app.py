eb4326370527d5a9e896a0755d2c512c416c459a
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-22 23:10:45 +0530

    Chore: Add Task 1,2,3 of Level-1 for Web App

--- app.py
+++ app.py
@@ -1,1 +1,50 @@
-"Streamlit Web App" 
+import streamlit as st
+# Importing the basic libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Define the tasks for each level
+tasks = {
+    'Level 1': ['Task 1', 'Task 2', 'Task 3'],
+    'Level 2': ['Task 1', 'Task 2', 'Task 3'],
+    'Level 3': ['Task 1', 'Task 2', 'Task 3']
+}
+
+# Create the Streamlit web app
+def main():
+    st.sidebar.title('Navigation')
+    selected_level = st.sidebar.selectbox('Select Level', list(tasks.keys()))
+
+    st.title(f'{selected_level}')
+    for task in tasks[selected_level]:
+        
+        if task == 'Task 1' and selected_level == 'Level 1':
+            ### Task 1: Data Exploration and Preprocessing
+            st.markdown('### Task 1: Data Exploration and Preprocessing')
+            st.write('---')
+            
+            st.write('Load the dataset and identify the number of rows and columns.')
+            df = pd.read_csv("./data/data.csv")
+            st.write(df.head())
+            st.write(f"Number of rows: {df.shape[0]}")
+            st.write(f"Number of columns: {df.shape[1]}")
+            st.write('---')
+            
+            st.write('Check for missing values in each column and handle them.')
+            st.write(df.isnull().sum())
+            st.write('---')
+            
+            st.write('Perform data type conversion if necessary.')
+            df['Votes'] = df['Votes'].astype(str).str.replace(',', '').astype(int)
+            st.write(f"The Data processed is:{df['Votes']}")
+            st.write('---')
+            
+            st.write('Analyse the distribution of the target variable (“Aggregate rating”) and identify any class imbalances.')    
+            
+
+    st.write('---')    
+
+if __name__ == '__main__':
+    main()
